b48758f24a5e1ee350d34f2be6ba81c72ed14bac
Bilal El Moudden <belel22@jaumebalmes.net>
2023-10-03 19:53:07 +0200

    hecho por bilal

--- index.php
+++ index.php
@@ -6,10 +6,31 @@
     <title>Practica3 M07 PHP</title>
 </head>
 <body>
-    
-    <php>
+
+    <?php
+        $restaurantTaules = array();
+
+        for ($i = 1; $i <= 10; $i++) {
+            $comensals = rand(0, 5); // Simular comensales aleatorios
+            $restaurantTaules[$i] = $comensals;
+
+            echo "Taula $i ";
+            if ($comensals == 0) {
+                echo "está vacía";
+            } elseif ($comensals == 5) {
+                echo "está llena";
+            } else {
+                echo "tiene $comensals comensals";
+            }
+            echo "<br>";
+            echo "<br>";
+        }
         
-    </php>
+    ?>
+
+    
+
+
 
 </body>
 </html>
